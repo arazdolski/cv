387d5801d4cce3776b364e5bc9e37fac2efff210
Aleksandr Razdolski <aleksandr.razdolski@gmail.com>
2023-09-21 17:16:34 +0300

    Set og:image tag again

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="CV" />
         <meta name="author" content="Aleksandr Razdolski" />
         <meta property="og:title" content="arazdolski.xyz" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.arazdolski.xyz/" />
         <meta property="og:description" content="CV" />
+        <meta property="og:image" content="/opengraph-image.png" />
       </Head>
       <body className="dark:bg-slate-950">
         <Main />
