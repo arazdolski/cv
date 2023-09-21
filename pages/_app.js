d54b1a3205897f6db9ad20cc50d4cdfddbf34148
Aleksandr Razdolski <aleksandr.razdolski@gmail.com>
2023-09-22 00:17:30 +0300

    Update metatags, add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import Layout from "@/components/Layout";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Aleksandr Razdolski - CV</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,32 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
         <meta name="description" content="CV" />
         <meta name="author" content="Aleksandr Razdolski" />
-        <meta property="og:title" content="arazdolski.xyz" />
+        <meta property="og:url" content="https://arazdolski.xyz" />
+        <meta
+          property="og:title"
+          content="Aleksandr Razdolski - CV"
+          key="title"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.arazdolski.xyz/" />
-        <meta property="og:description" content="CV" />
-        <meta property="og:image" content="/opengraph-image.png" />
+        <meta
+          property="og:description"
+          content="Site Reliability Engineer at Bolt. Located in Tallinn, Estonia. Check his CV."
+          key="description"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="Aleksandr Razdolski - CV"
+          key="title"
+        />
+        <meta
+          name="twitter:description"
+          content="Site Reliability Engineer at Bolt. Located in Tallinn, Estonia. Check his CV."
+          key="description"
+        />
       </Head>
       <body className="dark:bg-slate-950">
         <Main />
