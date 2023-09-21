6ddc57f7c5cfd515b02c3974b4de98d70668dc57
Aleksandr Razdolski <aleksandr.razdolski@gmail.com>
2023-09-21 16:57:49 +0300

    Add favicon, icon, apple-icon and metadata

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="description" content="CV" />
+        <meta name="author" content="Aleksandr Razdolski" />
+        <meta property="og:title" content="arazdolski.xyz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.arazdolski.xyz/" />
+        <meta property="og:description" content="CV" />
+        <meta property="og:image" content="http://keeb.ee/public/icon.svg" />
+      </Head>
       <body className="dark:bg-slate-950">
         <Main />
         <NextScript />
